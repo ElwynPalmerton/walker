4e806f01a36920cbc0c342fafc99061ace7a6f1f
Elwyn Palmerton <elwyn.palmerton@gmail.com>
2020-02-14 00:44:59 -0500

    changed master

--- sketch.js
+++ sketch.js
@@ -1,36 +1,24 @@
-let walker;
+//let walker;
+
+//Changed master.
+
+//Created Git repository with the branch "Gaussian."
 let walkers = [];
-let qty = 110;
+let qty = 1000;
 
 function setup() {
   createCanvas(displayWidth, displayHeight);
 
   for (let x = 0; x < qty; x++) {
     let hue = random(360);
-    let saturation = random(50);
-    let lightness = random(50);
-    let alpha = random(0.5);
-    walker = new Walker(hue, saturation, lightness, alpha);
-    walkers.push(walker);
-  }
+    let saturation = random(80);
+    let lightness = random(80);
+    let alpha = random(1.0);
+    // let hue = random(360);
+    // let saturation = 50;
+    // let lightness = 100;
+    // let alpha = 1;
 
-  for (let x = 0; x < qty / 5; x++) {
-    let hue = 330 + random(30);
-    if (random(100) > 50) {
-      hue = random(90);
-    }
-    let saturation = random(50) + 40;
-    let lightness = random(50) + 40;
-    let alpha = random(1);
-    walker = new Walker(hue, saturation, lightness, alpha);
-    walkers.push(walker);
-  }
-
-  for (let x = 0; x < qty; x++) {
-    let hue = 228;
-    let saturation = 100;
-    let lightness = 10;
-    let alpha = 1;
     walker = new Walker(hue, saturation, lightness, alpha);
     walkers.push(walker);
   }
@@ -39,7 +27,7 @@ function setup() {
   background(228, 100, 9, 1);
   setInterval(() => {
     background(228, 80, 20, 0.03);
-  }, 500)
+  }, 500);
 }
 
 function draw() {
--- walker.js
+++ walker.js
@@ -1,7 +1,7 @@
 class Walker {
   constructor(hue, saturation, lightness, alpha) {
-    this.x = width / 2;
-    this.y = height / 2;
+    this.x = randomGaussian(width / 2, 200);
+    this.y = randomGaussian(height / 2, 200);
     this.px = this.x;
     this.py = this.y;
 
@@ -18,22 +18,27 @@ class Walker {
     line(this.x, this.y, this.px, this.py);
     this.px = this.x;
     this.py = this.y;
+
+    // fill(250);
+    // noStroke();
+    // ellipse(750, 400, 20, 20);
   }
 
+
+
+  
   step() {
     let dist = 5;
     let leap = 20;
 
     let chance = random(100);
     if (chance < 99) {
-      this.x += random(-dist, dist);
-      this.y += random(-dist, dist);
+      this.x += randomGaussian(0, 5);
+      this.y += randomGaussian(0, 5);
     } else {
       this.x += random(-leap, leap);
       this.y += random(-leap, leap);
     }
-
-
 
     this.x = constrain(this.x, 0, width - 1);
     this.y = constrain(this.y, 0, height - 1);
